16d249721cbc00bfbe52fbb73f841445e8b662c5
Kai Marshland <kaimarshland@gmail.com>
2016-11-20 00:06:15 -0800

    Made it edit routes directly

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
-HerokuSsl::Engine.routes.draw do
+Rails.application.routes.draw do
 
-  get '.well-known/acme-challenge/:challenge' => 'heroku_ssl#challenge'
+  get '.well-known/acme-challenge/:challenge' => 'heroku_ssl/heroku_ssl#challenge'
 
 end
