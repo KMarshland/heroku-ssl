cbb70e9eae1c027862e3bd8827d5d90bcafd3abb
Kai Marshland <kaimarshland@gmail.com>
2016-11-19 19:26:21 -0800

    Cleaned up legacy code

--- lib/heroku_ssl/ssl.rb
+++ lib/heroku_ssl/ssl.rb
@@ -10,10 +10,43 @@ module HerokuSSL
       'https://acme-v01.api.letsencrypt.org/'
     end
 
+    def redis_instance
+
+    end
+
+    # Where the certificates are stored
+    def cert_directory
+      Rails.root.join('certs')
+    end
+
+    def write(filename, content)
+      FileUtils.mkdir_p cert_directory
+
+      File.write(cert_directory.join(filename), content)
+    end
+
+    def read(filename)
+      FileUtils.mkdir_p cert_directory
+
+      return nil unless File.exists? cert_directory.join(filename)
+
+      File.read cert_directory.join(filename)
+    end
+
+    def gen_unless_exists(filename)
+      existing = read filename
+      return existing if existing.present?
+
+      created = yield filename
+      write filename, created
+
+      created
+    end
+
     #forcibly regenerates the account private key
     def regenerate_private_key
       @private_key = OpenSSL::PKey::RSA.new(4096)
-      write_to_s3("#{Rails.env}/account.pem", @private_key.export)
+      write('account.pem', @private_key.export)
 
       @private_key
     end
@@ -22,7 +55,7 @@ module HerokuSSL
     def private_key
       return @private_key if @private_key.present?
 
-      pem = read_from_s3 "#{Rails.env}/account.pem"
+      pem = read "#{Rails.env}/account.pem"
       if pem.present?
         @private_key = OpenSSL::PKey::RSA.new(pem)
       else
@@ -45,14 +78,14 @@ module HerokuSSL
     end
 
     #adds a contact for a domain
-    def register(email='kai@leolabs.space')
+    def register(email)
       # If the private key is not known to the server, we need to register it for the first time.
       registration = client.register(contact: "mailto:#{email}")
 
       # You may need to agree to the terms of service (that's up the to the server to require it or not but boulder does by default)
       registration.agree_terms
     rescue Acme::Client::Error::Malformed => e
-      if e.message == "Registration key is already in use"
+      if e.message == 'Registration key is already in use'
         puts 'Already registered'
       else
         raise e
@@ -60,6 +93,14 @@ module HerokuSSL
     end
 
     def authorize(domain)
+      if domain.is_a? Array
+        domain.each do |dom|
+          authorize dom
+        end
+
+        return
+      end
+
       authorization = client.authorize(domain: domain)
 
       return if authorization.status == 'valid'
@@ -85,14 +126,7 @@ module HerokuSSL
       end
     end
 
-    def rename_old_certificates
-      %w[privkey cert chain fullchain].each do |name|
-        existing = read_from_s3 "#{Rails.env}/#{name}.pem"
-        write_to_s3("#{Rails.env}/#{name}_old.pem", existing) if existing.present?
-      end
-    end
-
-    def request_certificate(domain='api.leolabs.space')
+    def request_certificate(domain)
       authorize domain
 
       csr = Acme::Client::CertificateRequest.new(names: [domain])
@@ -102,33 +136,19 @@ module HerokuSSL
       # OpenSSL::X509::Request should work too.
       certificate = client.new_certificate(csr)
 
-      rename_old_certificates
-
-      write_to_s3("#{Rails.env}/privkey.pem", certificate.request.private_key.to_pem)
-      write_to_s3("#{Rails.env}/cert.pem", certificate.to_pem)
-      write_to_s3("#{Rails.env}/chain.pem", certificate.chain_to_pem)
-      write_to_s3("#{Rails.env}/fullchain.pem", certificate.fullchain_to_pem)
-    end
-
-    def get_certificates
-      [:privkey, :fullchain].each do |cert|
-        pem = read_from_s3 "#{Rails.env}/#{cert}.pem"
-        if pem.present?
-          File.write(Rails.root.join('certs', "#{cert}.pem"), pem)
-        else
-          request_certificate
-        end
-      end
-
-      #Generate fresh DH params
-      gen_if_not_exist 'dhparam.pem', lambda {|path|
-        "openssl dhparam -out #{path} 4096"
+      {
+          privkey: certificate.request.private_key.to_pem,
+          cert: certificate.to_pem,
+          chain: certificate.chain_to_pem,
+          fullchain: certificate.fullchain_to_pem
       }
 
     end
 
     def create_dh_params
-
+      gen_unless_exists 'dhparam.pem' do |filename|
+        `openssl dhparam -out #{filename} 4096`
+      end
     end
 
   end
